aa3467af8d28fcc8ff8c6c8d68fa18807c34bad0
Aaron Parecki <aaron@parecki.com>
2018-04-05 07:32:40 -0700

    move <form> markup higher to make it look a little better

--- templates/indieauth-authenticate-form.php
+++ templates/indieauth-authenticate-form.php
@@ -6,20 +6,19 @@ login_header(
 	$errors
 );
 ?>
+<form method="post" action="<?php echo $url; ?>">
 <div class="login-info">
 <?php echo get_avatar( $current_user->ID, '78' ); ?>
 <?php
 	printf(
-		'<p>' . __( 'The app <strong>%1$s</strong> would like to sign you in using the credentials of <strong>%2$s</strong>.', 'indieauth' ) . '</p>',
+		'<p>' . __( 'The app <strong>%1$s</strong> would like to sign you in as <strong>%2$s</strong>.', 'indieauth' ) . '</p>',
 		$client_id,
 		$current_user->user_url
 	);
 ?>
 </div>
 <br />
-
 <p class="submit">
-<form method="post" action="<?php echo $url; ?>">
 <?php // Hook to allow adding to form
 	add_action( 'indieauth_authentication_form', $current_user->user_id, $class_id );
 ?>
@@ -30,6 +29,6 @@ login_header(
         <input type="hidden" name="response_type" value="<?php echo $response_type; ?>" />
                 <button name="wp-submit" value="authorize" class="button button-primary button-large"><?php _e( 'Authenticate', 'indieauth' ); ?></button>
                 <a name="wp-submit" value="cancel" class="button button-large" href="<?php echo home_url(); ?>"><?php _e( 'Cancel', 'indieauth' ); ?></a>
-</form>
 </p>
+</form>
 <p class="redirect-info"><?php printf( __( 'You will be redirected to <code>%1$s</code> after authenticating.', 'indieauth' ), $redirect_uri ); ?></p>
