9e96ef0d707a620afe824943d731c089ea202eaf
Matthias Pfefferle <matthias@pfefferle.org>
2018-04-08 21:40:02 +0200

    Update indieauth-login-form.php

--- templates/indieauth-login-form.php
+++ templates/indieauth-login-form.php
@@ -9,18 +9,14 @@ login_header(
 <form name="loginform" id="loginform" action="<?php add_query_arg( 'action', 'indielogin', wp_login_url() ); ?>" method="post">
 <div class="login-info">
 <p><?php _e( 'Sign in with your domain', 'indieauth' ); ?></p>
-<br />
-	<input type="url" name="indieauth_identifier" placeholder="<?php _e( 'https://example.com', 'indieauth' ); ?>" />
-<br />
+	<input class="input" type="url" name="indieauth_identifier" placeholder="<?php _e( 'https://example.com', 'indieauth' ); ?>" />
 </div>
 <p class="submit">
 <?php
 	// Hook to allow adding to form
 	do_action( 'indieauth_login_form' );
 ?>
-<br />
 	<input class="input" type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php _e( 'Sign in', 'indieauth' ); ?>" />
 </p>
-<br />
 <a href="https://indieauth.net/" target="_blank"><?php _e( 'Learn about IndieAuth', 'indieauth' ); ?></a>
 </form>
