c1371acc062d84fae20a363e855d53c6575be48b
David Shanske <david@shanske.com>
2018-05-29 22:27:37 -0500

    Change message to web sign-in

--- templates/indieauth-domain-login.php
+++ templates/indieauth-domain-login.php
@@ -1,5 +1,5 @@
 <p style="margin-bottom: 8px;">
 	<label for="indieauth_identifier">
 	<a href="<?php echo add_query_arg( 'action', 'websignin', wp_login_url() ); ?>">
-	<?php _e( 'Sign in with your Domain', 'indieauth' ); ?></a>
+	<?php _e( 'Web Sign-In', 'indieauth' ); ?></a>
 </p>
