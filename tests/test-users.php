e1e4a74849ad69a1f3e57555632e7083d71bad3f
David Shanske <david@shanske.com>
2018-07-31 19:53:52 -0500

    Conflict adjustment

--- tests/test-users.php
+++ tests/test-users.php
@@ -18,13 +18,13 @@ class UsersTest extends WP_UnitTestCase {
 	// Test Getting the Author URL thorugh get_user_by_identifier
 	public function test_authorurl() {
 		$result = get_user_by_identifier( get_author_posts_url( static::$author_id ) );
-		$this->assertSame( $result, static::$author_id );
+		$this->assertSame( $result->ID, static::$author_id );
 	}
 
 	// Test Getting the Author URL through the url_to_author function directly
 	public function test_urltoauthor() {
 		$result = url_to_author( get_author_posts_url( static::$author_id ) );
-		$this->assertSame( $result, static::$author_id );
+		$this->assertSame( $result->ID, static::$author_id );
 	}
 
 	// Test Retrieving ID using the user_url
