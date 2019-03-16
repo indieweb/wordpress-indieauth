a43638654aef734f15488e3f5b971e55a5f081d4
David Shanske <david@shanske.com>
2019-03-16 23:15:16 +0000

    Remove errant period

--- includes/class-indieauth-authorization-endpoint.php
+++ includes/class-indieauth-authorization-endpoint.php
@@ -79,7 +79,7 @@ class IndieAuth_Authorization_Endpoint {
 			'channels' => __( 'Allows the application to manage channels', 'indieauth' ),
 			'save'     => __( 'Allows the application to save content for later retrieval', 'indieauth' ),
 			// Profile
-			'profile'  => __( 'Returns a complete profile to the application. Without this only a display name, avatar, and url will be returned.', 'indieauth' ),
+			'profile'  => __( 'Returns a complete profile to the application. Without this only a display name, avatar, and url will be returned', 'indieauth' ),
 		);
 		if ( 'all' === $scope ) {
 			return $scopes;
