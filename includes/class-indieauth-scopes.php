14970ce77c5c86f063d4904c409fbf654c3a24a8
David Shanske <david@shanske.com>
2020-02-01 20:34:24 +0000

    Switch the built in load over to the scopes class as should not be dependent on the authorization endpoint

--- includes/class-indieauth-scopes.php
+++ includes/class-indieauth-scopes.php
@@ -8,6 +8,137 @@ class IndieAuth_Scopes {
 
 	public function __construct() {
 		$this->scopes = array();
+		$this->register_builtin_scopes();
+	}
+
+	/**
+	 * Register Scopes
+	 */
+	public function register_builtin_scopes() {
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'create',
+				__( 'Allows the application to create posts and upload to the Media Endpoint', 'indieauth' ),
+				array(
+					'edit_posts',
+					'edit_published_posts',
+					'delete_posts',
+					'publish_posts',
+					'upload_files',
+					'read',
+				)
+			)
+		);
+
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'draft',
+				__( 'Allows the application to create draft posts only.', 'indieauth' ),
+				array(
+					'edit_posts',
+				)
+			)
+		);
+
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'update',
+				__( 'Allows the application to update posts', 'indieauth' ),
+				array(
+					'edit_published_posts',
+					'edit_others_posts',
+				)
+			)
+		);
+
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'delete',
+				__( 'Allows the application to delete or undelete posts', 'indieauth' ),
+				array(
+					'delete_posts',
+					'delete_published_posts',
+					'delete_others_posts',
+				)
+			)
+		);
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'media',
+				__( 'Allows the application to upload to the media endpoint', 'indieauth' ),
+				array(
+					'upload_files',
+				)
+			)
+		);
+
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'read',
+				__( 'Allows the application read access to Microsub channels', 'indieauth' ),
+				array(
+					'read',
+				)
+			)
+		);
+
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'follow',
+				__( 'Allows the application to manage a Microsub following list', 'indieauth' ),
+				array(
+					'read',
+				)
+			)
+		);
+
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'mute',
+				__( 'Allows the application to mute and unmute users in a Microsub channel', 'indieauth' ),
+				array(
+					'read',
+				)
+			)
+		);
+
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'block',
+				__( 'Allows the application to block users in a Microsub channel', 'indieauth' ),
+				array(
+					'read',
+				)
+			)
+		);
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'channels',
+				__( 'Allows the application to manage Microsub channels', 'indieauth' ),
+				array(
+					'read',
+				)
+			)
+		);
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'save',
+				__( 'Allows the application to save content for later retrieval', 'indieauth' ),
+				array(
+					'read',
+				)
+			)
+		);
+
+		$this->scopes->register_scope(
+			new IndieAuth_Scope(
+				'profile',
+				__( 'Returns a complete profile to the application as part of the IndieAuth response. Without this only a display name, avatar, and url will be returned', 'indieauth' ),
+				array(
+					'read',
+				)
+			)
+		);
 	}
 
 	public function register_scope( $scope ) {
