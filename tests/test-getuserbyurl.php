8f0dc1f14c2a1dbd9f1c884c42f7135fe2b21c9d
David Shanske <david@shanske.com>
2020-10-06 02:50:43 +0000

    Remove user url test as no longer part of the plugin

--- tests/test-getuserbyurl.php
+++ tests/test-getuserbyurl.php
@@ -26,12 +26,4 @@ class UsersTest extends WP_UnitTestCase {
 		$result = url_to_author( get_author_posts_url( static::$author_id ) );
 		$this->assertSame( $result->ID, static::$author_id );
 	}
-
-	// Test Retrieving ID using the user_url
-	public function test_userurl() {
-		$this->factory->user->create_many( 4 );
-		wp_update_user( array( 'ID' => 4, 'user_url' => 'http://example.uk' ) );
-		$result = get_user_by_identifier( 'http://example.uk' );
-		$this->assertSame( $result->ID, 4 );
-	}
 }
