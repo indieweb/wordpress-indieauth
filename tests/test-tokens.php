601377bcf35249031cad6061e6ec18d8e3d1c2cb
David Shanske <david@shanske.com>
2020-12-15 01:43:28 +0000

    Test expired tokens

--- tests/test-tokens.php
+++ tests/test-tokens.php
@@ -11,4 +11,14 @@ class TokensTest extends WP_UnitTestCase {
 		$this->assertEquals( $token, $get );
 	}
 
+
+	public function test_expired_token() {
+		$user_id = self::factory()->user->create();
+		$tokens = new Token_User( '_indieauth_code_', $user_id );
+		$token = array( 'foo' => 'foo', 'bar' => 'bar' );
+		$key = $tokens->set( $token, -30 );
+		$get = $tokens->get( $key );
+		$this->assertFalse( $get );
+	}
+
 }
