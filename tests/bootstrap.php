d04a89dd4936c83064dba1f370d8ab9e80574d3c
David Shanske <david@shanske.com>
2021-09-13 02:47:15 +0000

    Define unit test constant that will disable client discovery during testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,8 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
+define( 'INDIEAUTH_UNIT_TESTS', 1 );
+
 if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
