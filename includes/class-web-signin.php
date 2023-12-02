901a0a1f86140b11c34be4f044c118dff4732d3c
David Shanske <david@shanske.com>
2023-12-02 17:41:55 +0000

    Remove ability to login with URL plus password

--- includes/class-web-signin.php
+++ includes/class-web-signin.php
@@ -8,12 +8,9 @@ class Web_Signin {
 		add_action( 'init', array( $this, 'settings' ) );
 
 		add_action( 'login_form', array( $this, 'login_form' ) );
-		add_filter( 'login_form_defaults', array( $this, 'login_form_defaults' ), 10, 1 );
-		add_filter( 'gettext', array( $this, 'register_text' ), 10, 3 );
 		add_action( 'login_form_websignin', array( $this, 'login_form_websignin' ) );
 
 		add_action( 'authenticate', array( $this, 'authenticate' ), 20, 2 );
-		add_action( 'authenticate', array( $this, 'authenticate_url_password' ), 10, 3 );
 	}
 
 	public function settings() {
@@ -226,73 +223,6 @@ class Web_Signin {
 	}
 
 
-
-	/**
-	 * Authenticate user to WordPress using URL and Password
-	 */
-	public function authenticate_url_password( $user, $url, $password ) {
-		if ( $user instanceof WP_User ) {
-			return $user;
-		}
-		if ( empty( $url ) || empty( $password ) ) {
-			if ( is_wp_error( $user ) ) {
-				return $user;
-			}
-			if ( is_oauth_error( $user ) ) {
-				return $user->to_wp_error();
-			}
-			$error = new WP_Error();
-
-			if ( empty( $url ) ) {
-				$error->add( 'empty_username', __( '<strong>ERROR</strong>: The URL field is empty.', 'indieauth' ) ); // Uses 'empty_username' for back-compat with wp_signon()
-			}
-
-			if ( empty( $password ) ) {
-				$error->add( 'empty_password', __( '<strong>ERROR</strong>: The password field is empty.', 'indieauth' ) );
-			}
-
-			return $error;
-		}
-
-		if ( ! wp_http_validate_url( $url ) ) {
-			return $user;
-		}
-		$user = get_user_by_identifier( $url );
-
-		if ( ! $user ) {
-			return new WP_Error(
-				'invalid_url',
-				__( '<strong>ERROR</strong>: Invalid URL.', 'indieauth' ) .
-				' <a href="' . wp_lostpassword_url() . '">' .
-				__( 'Lost your password?', 'indieauth' ) .
-				'</a>'
-			);
-		}
-
-		/** This filter is documented in wp-includes/user.php */
-		$user = apply_filters( 'wp_authenticate_user', $user, $password );
-
-		if ( is_wp_error( $user ) ) {
-			return $user;
-		}
-
-		if ( ! wp_check_password( $password, $user->user_pass, $user->ID ) ) {
-			return new WP_Error(
-				'incorrect_password',
-				sprintf(
-					/* translators: %s: url */
-					__( '<strong>ERROR</strong>: The password you entered for the URL %s is incorrect.', 'indieauth' ),
-					'<strong>' . $url . '</strong>'
-				) .
-				' <a href="' . wp_lostpassword_url() . '">' .
-				__( 'Lost your password?', 'indieauth' ) .
-				'</a>'
-			);
-		}
-
-		return $user;
-	}
-
 	/**
 	 * render the login form
 	 */
@@ -301,18 +231,6 @@ class Web_Signin {
 		if ( 1 === (int) get_option( 'indieauth_show_login_form' ) ) {
 			load_template( $template );
 		}
-	}
-
-	public function login_form_defaults( $defaults ) {
-		$defaults['label_username'] = __( 'Username, Email Address, or URL', 'indieauth' );
-		return $defaults;
-	}
-
-	public function register_text( $translated_text, $untranslated_text, $domain ) {
-		if ( 'Username or Email Address' === $untranslated_text ) {
-			$translated_text = __( 'Username, Email Address, or URL', 'indieauth' );
-		}
-		return $translated_text;
 	}
 
 	public function login_form_websignin() {
