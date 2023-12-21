49b1e2886150a9aa968d1f4fc804ba5c5c255e0b
David Shanske <david@shanske.com>
2023-12-21 17:55:49 +0000

    Move exit function to construct to try to fix issue reported using localhost

--- includes/class-indieauth-client-discovery.php
+++ includes/class-indieauth-client-discovery.php
@@ -13,6 +13,18 @@ class IndieAuth_Client_Discovery {
 		if ( defined( 'INDIEAUTH_UNIT_TESTS' ) ) {
 			return;
 		}
+		// Validate if this is an IP address
+		$ip         = filter_var( wp_parse_url( $client_id, PHP_URL_HOST ), FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 );
+		$donotfetch = array(
+			'127.0.0.1',
+			'0000:0000:0000:0000:0000:0000:0000:0001',
+			'::1',
+		);
+
+		// If this is an IP address on the donotfetch list then do not fetch.
+		if ( ( $ip && ! in_array( $ip, $donotfetch, true ) || 'localhost' === wp_parse_url( $client_id, PHP_URL_HOST ) ) ) {
+			return;
+		}
 
 		$this->html = self::parse( $client_id );
 		if ( is_wp_error( $this->html ) ) {
@@ -30,20 +42,6 @@ class IndieAuth_Client_Discovery {
 	}
 
 	private function fetch( $url ) {
-
-		// Validate if this is an IP address
-		$ip         = filter_var( wp_parse_url( $url, PHP_URL_HOST ), FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 );
-		$donotfetch = array(
-			'127.0.0.1',
-			'0000:0000:0000:0000:0000:0000:0000:0001',
-			'::1',
-		);
-
-		// If this is an IP address ion the donotfetch list then do not fetch.
-		if ( $ip && ! in_array( $ip, $donotfetch ) ) {
-			return new WP_Error( 'do_not_fetch', __( 'Client Identifier is localhost', 'indieauth' ) );
-		}
-
 		$wp_version = get_bloginfo( 'version' );
 		$user_agent = apply_filters( 'http_headers_useragent', 'WordPress/' . $wp_version . '; ' . get_bloginfo( 'url' ) );
 		$args       = array(
