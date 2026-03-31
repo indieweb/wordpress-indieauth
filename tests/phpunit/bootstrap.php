383c73007dc0cc4b0ba4f93996fb21ba0e435ff9
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-03-31 16:17:46 +0200

    Fix ClassicPress test bootstrap to find classicpress-tests-lib directory

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -18,6 +18,14 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
+// Fall back to ClassicPress tests directory.
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	$_tests_dir = getenv( 'CP_TESTS_DIR' );
+	if ( ! $_tests_dir ) {
+		$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/classicpress-tests-lib';
+	}
+}
+
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	exit( 1 );
