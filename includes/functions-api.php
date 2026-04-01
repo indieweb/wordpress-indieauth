60e74c5f23695cf79ca89ae1803145e6db61ba0a
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-04-01 11:18:20 +0200

    Add missing backward-compat function wrappers
    All namespaced utility functions (find_rels, url_to_author, pkce_verifier,
    base64_urlencode, normalize_url, build_url, add_query_params_to_url,
    rest_is_valid_url, get_oauth_error, is_oauth_error,
    wp_error_to_oauth_response) now have global wrappers in functions-api.php.

--- includes/functions-api.php
+++ includes/functions-api.php
@@ -192,3 +192,140 @@ if ( ! function_exists( 'get_url_from_user' ) ) {
 		return IndieAuth\get_url_from_user( $user_id );
 	}
 }
+
+if ( ! function_exists( 'find_rels' ) ) {
+	/**
+	 * Find rels on a given URL.
+	 *
+	 * @param string       $me        URL.
+	 * @param string|array $endpoints Endpoints to search for.
+	 * @return bool|array|string Results or false.
+	 */
+	function find_rels( $me, $endpoints = null ) {
+		return IndieAuth\find_rels( $me, $endpoints );
+	}
+}
+
+if ( ! function_exists( 'url_to_author' ) ) {
+	/**
+	 * Try to determine the author from a URL.
+	 *
+	 * @param string $url Permalink to check.
+	 * @return WP_User|null User or null.
+	 */
+	function url_to_author( $url ) {
+		return IndieAuth\url_to_author( $url );
+	}
+}
+
+if ( ! function_exists( 'pkce_verifier' ) ) {
+	/**
+	 * Verify PKCE code challenge.
+	 *
+	 * @param string $code_challenge Code challenge.
+	 * @param string $code_verifier  Code verifier.
+	 * @param string $method         Challenge method.
+	 * @return bool Whether verification passed.
+	 */
+	function pkce_verifier( $code_challenge, $code_verifier, $method ) {
+		return IndieAuth\pkce_verifier( $code_challenge, $code_verifier, $method );
+	}
+}
+
+if ( ! function_exists( 'base64_urlencode' ) ) {
+	/**
+	 * URL-safe base64 encode.
+	 *
+	 * @param string $data String to encode.
+	 * @return string Encoded string.
+	 */
+	function base64_urlencode( $data ) {
+		return IndieAuth\base64_urlencode( $data );
+	}
+}
+
+if ( ! function_exists( 'normalize_url' ) ) {
+	/**
+	 * Normalize a URL.
+	 *
+	 * @param string $url       URL to normalize.
+	 * @param bool   $force_ssl Whether to force SSL.
+	 * @return string|false Normalized URL or false.
+	 */
+	function normalize_url( $url, $force_ssl = false ) {
+		return IndieAuth\normalize_url( $url, $force_ssl );
+	}
+}
+
+if ( ! function_exists( 'add_query_params_to_url' ) ) {
+	/**
+	 * Add query parameters to a URL.
+	 *
+	 * @param array  $args Query parameters.
+	 * @param string $url  URL.
+	 * @return string URL with parameters.
+	 */
+	function add_query_params_to_url( $args, $url ) {
+		return IndieAuth\add_query_params_to_url( $args, $url );
+	}
+}
+
+if ( ! function_exists( 'build_url' ) ) {
+	/**
+	 * Inverse of parse_url.
+	 *
+	 * @param array $parsed_url Parsed URL components.
+	 * @return string URL.
+	 */
+	function build_url( $parsed_url ) {
+		return IndieAuth\build_url( $parsed_url );
+	}
+}
+
+if ( ! function_exists( 'rest_is_valid_url' ) ) {
+	/**
+	 * Validate URL for REST API.
+	 *
+	 * @param string $url URL to validate.
+	 * @return bool Whether valid.
+	 */
+	function rest_is_valid_url( $url ) {
+		return IndieAuth\rest_is_valid_url( $url );
+	}
+}
+
+if ( ! function_exists( 'get_oauth_error' ) ) {
+	/**
+	 * Get OAuth error from response.
+	 *
+	 * @param mixed $obj Response object or array.
+	 * @return IndieAuth\OAuth_Response|false OAuth response or false.
+	 */
+	function get_oauth_error( $obj ) {
+		return IndieAuth\get_oauth_error( $obj );
+	}
+}
+
+if ( ! function_exists( 'is_oauth_error' ) ) {
+	/**
+	 * Check if object is an OAuth error.
+	 *
+	 * @param mixed $obj Object to check.
+	 * @return bool True if OAuth error.
+	 */
+	function is_oauth_error( $obj ) {
+		return IndieAuth\is_oauth_error( $obj );
+	}
+}
+
+if ( ! function_exists( 'wp_error_to_oauth_response' ) ) {
+	/**
+	 * Convert WP_Error to OAuth response.
+	 *
+	 * @param WP_Error $error WordPress error.
+	 * @return IndieAuth\OAuth_Response|null OAuth response or null.
+	 */
+	function wp_error_to_oauth_response( $error ) {
+		return IndieAuth\wp_error_to_oauth_response( $error );
+	}
+}
