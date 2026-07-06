319603ee7db55c08adb8653ef00bc685c6ab49de
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-07-06 13:41:57 +0200

    Fully qualify function calls in the error path

--- templates/indieauth-authenticate-form.php
+++ templates/indieauth-authenticate-form.php
@@ -8,8 +8,8 @@ login_header(
 );
 $user_website = esc_url( get_url_from_user( $current_user->ID ) );
 if ( ! $user_website ) {
-	esc_html_e( 'The application cannot sign you in as WordPress cannot determine the current user', 'indieauth' );
-	login_footer();
+	\esc_html_e( 'The application cannot sign you in as WordPress cannot determine the current user', 'indieauth' );
+	\login_footer();
 	exit;
 }
 
